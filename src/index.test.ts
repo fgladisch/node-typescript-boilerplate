3d9f30524d8d9809f3487aea0dec9d9ee1bc923e
Felix A. Gladisch <felix@felixgladisch.de>
2019-06-25 10:54:12 +0200

    chore: new dependencies

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,9 +1,7 @@
-import { assert } from "chai";
-
 import { message } from ".";
 
 describe("Hello World", () => {
   it("should export 'Hello World'", () => {
-    assert.equal(message, "Hello World");
+    expect(message).toEqual("Hello World");
   });
 });
